b238a8681421dfece21860cf2d765726390b0090
web-Ali <38067660+web-Ali@users.noreply.github.com>
2022-11-06 12:14:14 +0300

    fix2

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test <code>src/App.tsx</code> and save to reload.
+          Test2 <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
